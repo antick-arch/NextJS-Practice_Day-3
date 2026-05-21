86a3cf65dfb9e75be60d39a76b0ee70074538e83
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-21 22:46:53 +0600

    Add Header component and Posts page; integrate daisyUI

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsPage = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <h2 className='text-4xl text-red-500 font-extrabold'>This is posts page</h2>
+        </div>
+    );
+};
+
+export default PostsPage;
