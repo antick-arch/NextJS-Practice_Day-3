0ba2d4fd0da4dffb4544597a2abf14d4e96df9a2
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-22 11:09:13 +0600

    Add ProductCard component and ProductPage to display products; update Header for navigation

---
+++ src/app/products/page.jsx
@@ -0,0 +1,22 @@
+import ProductCard from "@/components/ProductCard";
+
+const getProducts = async() =>{
+    const res = await fetch('http://localhost:3004/products');
+    return res.json();
+}
+
+const ProductPage = async() => {
+    const products = await getProducts();
+    return (
+        <div>
+            <h2>Total Products: {products.length}</h2>
+            <div className="grid grid-cols-4 gap-4 place-items-center">
+                {
+                    products.map((product)=><ProductCard key={product.id} product={product}></ProductCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ProductPage;
