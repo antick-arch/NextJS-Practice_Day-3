093dbdd0f677018e43b07dd7f83a6016c7261f40
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-22 10:36:01 +0600

    Refactor PostsPage to fetch and display post count from API

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+// const getPost = async()=>{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+// }
 
-const PostsPage = () => {
+const getPost = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if(!res.ok) throw new Error('failed to fetch data');
+    return res.json();
+}
+
+// const getPost = async()=>{
+//     try{
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+//     }
+//     catch(err){
+//         throw new Error ('failed to fetch data');
+//     }
+// }
+
+const PostsPage = async () => {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const posts = await res.json();
+    const posts = await getPost();
     return (
         <div className='flex justify-center items-center h-screen'>
-            <h2 className='text-4xl text-red-500 font-extrabold'>This is posts page</h2>
+            <h2 className='text-4xl text-red-500 font-extrabold'>{posts.length}</h2>
         </div>
     );
 };
